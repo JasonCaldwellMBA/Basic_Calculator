d4e8c9e086d99598974e04b4f4815460c7d0ee92
Jason Caldwell <jdcald13@gmail.com>
2017-12-22 17:17:07 -0700

    Add navbar and footer

--- public/calc.js
+++ public/calc.js
@@ -122,6 +122,10 @@ var displayKB = function(e){
   else if(e.key === 'C' || e.key === 'c'){
     clear();
   }
+  else if(e.which === 48 || e.keyCode === 48){
+    e.preventDefault(); // Not a form
+    clear();
+  }
   else {
     switch(e.key){
       case '/':
